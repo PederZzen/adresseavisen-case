7a96003ad91b5d9234963205b850a883273ca6df
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2025-04-29 12:11:55 +0200

    Added salesposter, logic and styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,17 @@
 const paragraphs = document.querySelectorAll("article p");
 const contentWrapper = document.querySelector(".content-wrapper");
 const body = document.querySelector("body");
-const product = null;
-
 let isLocked = false;
+let isMember = false;
 
-window.addEventListener("scroll", () => {
-  if (isLocked) return;
-
-  if (paragraphs[3].getBoundingClientRect().top < window.innerHeight) {
-    createSalesposter();
-    body.style.overflow = "hidden";
-    contentWrapper.style.filter = "blur(0.2rem)";
-    isLocked = true;
-  }
-});
+let product = {
+  name: "Digital",
+  price_firstmonth: "1",
+  price_full: "149",
+};
 
 function fetchData() {
-  fetch(" https://brukermarkedtest-5a3d.restdb.io/rest/products", {
+  fetch("https://brukermarkedtest-5a3d.restdb.io/rest/products", {
     method: "GET",
     headers: {
       "x-api-key": "680b5f8972702c5e96b3d2a4",
@@ -25,29 +19,55 @@ function fetchData() {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       product = data[0];
+      onScroll();
+    })
+    .catch((error) => {
+      console.error("Feil ved henting av API, bruker fallback");
+      onScroll();
     });
 }
 
-if (!isLocked) {
-  fetchData();
+function onScroll() {
+  window.addEventListener("scroll", () => {
+    if (isLocked) return;
+    if (paragraphs[3].getBoundingClientRect().top < window.innerHeight) {
+      createSalesposter(product);
+      body.style.overflow = "hidden";
+      contentWrapper.style.filter = "blur(0.2rem)";
+      isLocked = true;
+    }
+  });
 }
 
+fetchData();
+
 function createSalesposter(product) {
+  const productName =
+    product.name.charAt(0).toUpperCase() + product.name.slice(1);
   const salesposter = document.createElement("div");
   salesposter.id = "salesposter";
   salesposter.innerHTML = `
     <img src="../assets/illustration.png" alt="Trønder med bærbar pc"/>  
-    <h3><strong>${product}: </strong> Pluss medlem for tilgang</h3>
-    <p>Allerede medlem?</p>
-    <button class="btn btn-secondary" onClick="signIn()">Logg in</button>
+    <h3><span>${productName}: </span> Les med Pluss-tilgang</h3>
+    <button class="btn btn-primary">${product.price_firstmonth} kr første måned</button>
+    <small>Ordinær pris ${product.price_full},-/mnd</small>
+    <ul>
+      <li>Fri tilgang til Skinnvestpodden</li>
+      <li>50% avslag på Inderøysodd</li>
+      <li>Pluss mye, mye mer!</li>
+    </ul>
+    <div id="sign-in_container">
+      <p>Allerede medlem?</p>
+      <button class="btn btn-secondary" onClick="signIn()">Logg in</button>
+    </div>
   `;
   document.body.appendChild(salesposter);
 }
 
 function signIn() {
   body.style.overflow = "visible";
+  isMember = true;
   contentWrapper.style.filter = "blur(0)";
   document.querySelector("#salesposter").style.display = "none";
 }
